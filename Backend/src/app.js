e114eaebcdfac49da822a3ab82fdf59342c451e0
Faizan <sfaizan78692@gmail.com>
2024-03-25 07:53:29 +0530

    change cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://money-transfer-two.vercel.app",
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
