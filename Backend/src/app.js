8e29d31cdadfcb8797137af8d8de5f9936fa0d9f
Faizan <sfaizan78692@gmail.com>
2024-06-01 15:37:33 +0530

    allow static files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,6 +12,7 @@ app.use(
 );
 
 app.set("trust proxy", 1);
+app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" })); // from json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // from url
 app.use(cookieParser()); // for secure CRUD operations from server to browser
