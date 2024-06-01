153882776fb9ea7ce2465b0d05b8dd05287025b8
Faizan <sfaizan78692@gmail.com>
2024-06-01 14:21:14 +0530

    init

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(
 app.set("trust proxy", 1);
 app.use(express.json({ limit: "16kb" })); // from json
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // from url
-app.use(express.static("public")); // static files like pdf, image to store in temp
 app.use(cookieParser()); // for secure CRUD operations from server to browser
 
 // importing routes
@@ -26,13 +24,5 @@ import healthRouter from "./routes/health.route.js";
 app.use("/api/v2/users", userRouter);
 app.use("/api/v2/transactions", transactionRouter);
 app.use("/api/v2/health", healthRouter);
-
-const __dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
-});
 
 export { app };
